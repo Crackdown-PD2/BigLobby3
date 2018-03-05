084ce68243a3182032b592e0ce1cc6aa17346e5f
steam-test1 <34393637+steam-test1@users.noreply.github.com>
2018-03-05 19:47:19 +0000

    Update _clientnetworksession.lua

--- lua/lib/network/base/_clientnetworksession.lua
+++ lua/lib/network/base/_clientnetworksession.lua
@@ -9,7 +9,8 @@ function ClientNetworkSession:on_join_request_reply(...)
 	-- Get params we want based on if the func signature is correct
 	local reply = params[1]
 	local sender = #params==17 and params[17] -- Should be the last param
-	local num_players = sender and type(params[16])=='number' and params[16]
+	--local num_players = sender and type(params[16])=='number' and params[16]
+	local num_players = sender and params[16]
 
 	-- If the response is `1`(ok), set BigLobby to use host preference or 4 if
 	-- a regular lobby (num_players param is falsey).
