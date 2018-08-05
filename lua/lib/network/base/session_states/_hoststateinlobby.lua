25cb7b26286c5ee03b1417f363b073a41ed40557
steam-test1 <34393637+steam-test1@users.noreply.github.com>
2018-08-05 18:28:28 +0100

    Update _hoststateinlobby.lua

--- lua/lib/network/base/session_states/_hoststateinlobby.lua
+++ lua/lib/network/base/session_states/_hoststateinlobby.lua
@@ -1,36 +1,25 @@
 -- Unfortunately no clean way to modify this bit of code, so I have to include the
 -- original code with modified, could cause problems with other mods that would want to touch this function
 function HostStateInLobby:on_join_request_received(data, peer_name, client_preferred_character, dlcs, xuid, peer_level, peer_rank, gameversion, join_attempt_identifier, auth_ticket, sender)
+	
 	-- Number of players allowed to join the game(excluding the host)
 	local num_player_slots = BigLobbyGlobals:num_player_slots() - 1
-
-
-
-
+	
 	-- Original Code --
 	print("HostStateInLobby:on_join_request_received peer_level", peer_level, join_attempt_identifier, gameversion)
-	
+
 	if SystemInfo:platform() == Idstring("WIN32") then
 		peer_name = managers.network.account:username_by_id(sender:ip_at_index(0))
 	end
-	
+
 	if self:_has_peer_left_PSN(peer_name) then
 		print("this CLIENT has left us from PSN, ignore his request", peer_name)
-		
+
 		return
 	end
-	
+
 	local my_user_id = data.local_peer:user_id() or ""
-	--[[
-	if self:_is_kicked(data, peer_name, sender) then
-		print("YOU ARE IN MY KICKED LIST", peer_name)
-		self:_send_request_denied(sender, 2, my_user_id)
-		
-		return
-	end
-	--]]
-	
-	--ADDED
+
 	if self:_is_banned(peer_name, sender) then
 		self:_send_request_denied(sender, 9, my_user_id)
 
@@ -44,46 +33,42 @@ function HostStateInLobby:on_join_request_received(data, peer_name, client_prefe
 
 		return
 	end
-	--END ADDED
-	
+
 	if peer_level < Global.game_settings.reputation_permission then
 		self:_send_request_denied(sender, 6, my_user_id)
-		
+
 		return
 	end
-	
+
 	if gameversion ~= -1 and gameversion ~= managers.network.matchmake.GAMEVERSION then
 		self:_send_request_denied(sender, 7, my_user_id)
-		
+
 		return
 	end
-	
+
 	if data.wants_to_load_level then
 		self:_send_request_denied(sender, 0, my_user_id)
-		
+
 		return
 	end
-	
+
 	if not managers.network:session():local_peer() then
 		self:_send_request_denied(sender, 0, my_user_id)
-		
+
 		return
 	end
-	
+
 	local old_peer = data.session:chk_peer_already_in(sender)
-	
+
 	if old_peer then
 		if join_attempt_identifier ~= old_peer:join_attempt_identifier() then
 			data.session:remove_peer(old_peer, old_peer:id(), "lost")
 			self:_send_request_denied(sender, 0, my_user_id)
 		end
-		
+
 		return
 	end
 	-- End Original Code --
-
-
-
 
 	-- num_player_slots variable instead of hardcoded 3, removes enforced limit.
 	if table.size(data.peers) >= num_player_slots then
@@ -92,48 +77,55 @@ function HostStateInLobby:on_join_request_received(data, peer_name, client_prefe
 		return
 	end
 
-
-
-
 	-- Original Code --
 	print("[HostStateInLobby:on_join_request_received] new peer accepted", peer_name)
+
 	local character = managers.network:session():check_peer_preferred_character(client_preferred_character)
 	local xnaddr = ""
+
 	if SystemInfo:platform() == Idstring("X360") or SystemInfo:platform() == Idstring("XB1") then
 		xnaddr = managers.network.matchmake:external_address(sender)
 	end
-	local new_peer_id, new_peer
+
+	local new_peer_id, new_peer = nil
 	new_peer_id, new_peer = data.session:add_peer(peer_name, nil, true, false, false, nil, character, sender:ip_at_index(0), xuid, xnaddr)
+
 	if not new_peer_id then
 		print("there was no clean peer_id")
 		self:_send_request_denied(sender, 0, my_user_id)
+
 		return
 	end
+
 	new_peer:set_dlcs(dlcs)
 	new_peer:set_xuid(xuid)
 	new_peer:set_join_attempt_identifier(join_attempt_identifier)
-	local new_peer_rpc
-	if managers.network:protocol_type() == "TCP_IP" then
-		new_peer_rpc = managers.network:session():resolve_new_peer_rpc(new_peer, sender)
-	else
-		new_peer_rpc = sender
-	end
+
+	local new_peer_rpc = nil
+	new_peer_rpc = managers.network:protocol_type() == "TCP_IP" and managers.network:session():resolve_new_peer_rpc(new_peer, sender) or sender
+
 	new_peer:set_rpc(new_peer_rpc)
 	new_peer:set_ip_verified(true)
 	Network:add_client(new_peer:rpc())
+
 	if not new_peer:begin_ticket_session(auth_ticket) then
 		data.session:remove_peer(new_peer, new_peer:id(), "auth_fail")
 		self:_send_request_denied(sender, 8, my_user_id)
+
 		return
 	end
+
 	local ticket = new_peer:create_ticket()
+
 	new_peer:set_entering_lobby(true)
+
 	local level_index = tweak_data.levels:get_index_from_level_id(Global.game_settings.level_id)
 	local difficulty_index = tweak_data:difficulty_to_index(Global.game_settings.difficulty)
 	local job_id_index = 0
 	local job_stage = 0
 	local alternative_job_stage = 0
 	local interupt_job_stage_level_index = 0
+
 	if managers.job:has_active_job() then
 		job_id_index = tweak_data.narrative:get_index_from_job_id(managers.job:current_job_id())
 		job_stage = managers.job:current_stage()
@@ -141,49 +133,46 @@ function HostStateInLobby:on_join_request_received(data, peer_name, client_prefe
 		local interupt_stage_level = managers.job:interupt_stage()
 		interupt_job_stage_level_index = interupt_stage_level and tweak_data.levels:get_index_from_level_id(interupt_stage_level) or 0
 	end
+
 	local server_xuid = (SystemInfo:platform() == Idstring("X360") or SystemInfo:platform() == Idstring("XB1")) and managers.network.account:player_id() or ""
 	-- End Original Code --
-
-
-
-
+	
 	-- Appears orginally, but is modified to include the num_player_slots parameter
 	local params = {
-		1, 
-		new_peer_id, 
-		character, 
-		level_index, 
-		difficulty_index, 
+		1,
+		new_peer_id,
+		character,
+		level_index,
+		difficulty_index,
 		Global.game_settings.one_down,
-		1, 
-		data.local_peer:character(), 
-		my_user_id, 
-		Global.game_settings.mission, 
-		job_id_index, 
-		job_stage, 
-		alternative_job_stage, 
-		interupt_job_stage_level_index, 
-		server_xuid, 
-		ticket, 
+		1,
+		data.local_peer:character(),
+		my_user_id,
+		Global.game_settings.mission,
+		job_id_index,
+		job_stage,
+		alternative_job_stage,
+		interupt_job_stage_level_index,
+		server_xuid,
+		ticket,
 		BigLobbyGlobals:num_player_slots()
 	}
-	
+
 	new_peer:send("join_request_reply", unpack(params))
-
-
-
-
+	
 	-- Original Code --
 	new_peer:send("set_loading_state", false, data.session:load_counter())
+
 	if SystemInfo:platform() == Idstring("X360") or SystemInfo:platform() == Idstring("XB1") then
 		new_peer:send("request_player_name_reply", managers.network:session():local_peer():name())
 	end
+
 	managers.vote:sync_server_kick_option(new_peer)
 	self:_introduce_new_peer_to_old_peers(data, new_peer, false, peer_name, new_peer:character(), "remove", new_peer:xuid(), new_peer:xnaddr())
 	self:_introduce_old_peers_to_new_peer(data, new_peer)
 	self:on_handshake_confirmation(data, new_peer, 1)
 	managers.network:session():local_peer():sync_lobby_data(new_peer)
-	
+
 	if peer_rank > 0 then
 		managers.menu:post_event("infamous_player_join_stinger")
 	else
