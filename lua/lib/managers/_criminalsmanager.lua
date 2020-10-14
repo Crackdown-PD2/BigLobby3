fa634554465096e1cd0089c77df3f7df668feeeb
steam-test1 <34393637+steam-test1@users.noreply.github.com>
2020-10-14 08:03:21 +0100

    Temp fix for Breakfast in Tijuana escape crash

--- lua/lib/managers/_criminalsmanager.lua
+++ lua/lib/managers/_criminalsmanager.lua
@@ -1,4 +1,5 @@
 if Global.load_level == true and Global.game_settings.level_id == "vit" then return end
+if Global.load_level == true and Global.game_settings.level_id == "pex" then return end
 
 if BigLobbyGlobals.allow_more_bots_settings then
 	CriminalsManager.MAX_NR_TEAM_AI = BigLobbyGlobals:num_bot_slots()
