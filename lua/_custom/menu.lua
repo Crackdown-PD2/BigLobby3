c36b53a80e28557c2169deff9622e056498cce15
steam-test1 <34393637+steam-test1@users.noreply.github.com>
2020-11-01 20:36:30 +0000

    Beta 2.2
    Changed the stop all ai to not move during stealth
    Added chinese language by SummonHIM

--- lua/_custom/menu.lua
+++ lua/_custom/menu.lua
@@ -54,7 +54,20 @@ end
 
 function bkin_bl__menu:RegisterHooks()
 	Hooks:Add("LocalizationManagerPostInit", "LocalizationManagerPostInit__bkin_bl", function( loc )
-		loc:load_localization_file(BigLobbyGlobals.ModPath .. "l10n/en.json")
+		if not language_filename then
+			for _, filename in pairs(file.GetFiles(BigLobbyGlobals.ModPath .. 'l10n/')) do
+				local str = filename:match('^(.*).json$')
+				if str and Idstring(str) and Idstring(str):key() == SystemInfo:language():key() then
+					language_filename = filename
+					break
+				end
+			end
+		end
+
+		if language_filename then
+			loc:load_localization_file(BigLobbyGlobals.ModPath .. 'l10n/' .. language_filename)
+		end
+		loc:load_localization_file(BigLobbyGlobals.ModPath .. "l10n/english.json")
 	end)
 
 
--- lua/lib/managers/group_ai_states/_groupaistatebase.lua
+++ lua/lib/managers/group_ai_states/_groupaistatebase.lua
@@ -17,10 +17,13 @@ end
 
 Hooks:PostHook( GroupAIStateBase , "whisper_mode" , "GroupAIStateBasePostWhisperMode" , function( self )
 	for _, ai in pairs(managers.groupai:state():all_AI_criminals()) do
-	if self._whisper_mode == true and BigLobbyGlobals.auto_stop_all_bots_settings then
-		ai.unit:movement():set_should_stay(true)
-	elseif self._whisper_mode == false and BigLobbyGlobals.auto_stop_all_bots_settings then
-		ai.unit:movement():set_should_stay(false)
-	end
+		if self._whisper_mode == true and BigLobbyGlobals.auto_stop_all_bots_settings then
+			ai.unit:movement():play_redirect("crouch")
+			ai.unit:movement():action_request({
+				sync = true,
+				body_part = 1,
+				type = "idle"
+			})
+		end
 	end
 end )
