57a5e7d316dd0dc78e33ed8abcd52b852e208acc
steam-test1 <34393637+steam-test1@users.noreply.github.com>
2023-12-05 23:48:21 +0000

    Attempt at fixing 240.3 crashing

--- lua/lib/network/base/session_states/_hoststateinlobby.lua
+++ lua/lib/network/base/session_states/_hoststateinlobby.lua
@@ -163,7 +163,7 @@ function HostStateInLobby:on_join_request_received(data, peer_name, peer_account
 		return
 	end
 
-	local ticket = new_peer:create_ticket()
+	local ticket = new_peer:create_ticket(data.local_peer:account_id())
 
 	new_peer:set_entering_lobby(true)
 
